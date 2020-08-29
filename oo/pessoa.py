dcdac2ecf40fb963b38e240b684d1082ef5e211b
vladimir <vladimirmaciel@gmail.com>
2020-08-29 02:26:15 -0300

    sobrescrevendo o metodo comprimentar, sobrescirta de método

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -5,8 +5,9 @@ class Pessoa:
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
-    def cumprimentar(self):
-        return f'Olá {id(self)}'
+    @property
+    def comprimentar(self):
+        return f'Olá meu nome é  {self.nome}'
     @staticmethod
     def metodo_estatico():
         return 42
@@ -14,40 +15,47 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos{cls.olhos}'
 
+
 class Homem(Pessoa):
-    #sobrescrita de atributos
-    olhos = 2
-    pass
+    def comprimentar(self):
+        # comprimentar_da_classe=super().comprimentar
+        return f'{super().comprimentar}. Aperto de Mão'
+
+
+
 class Mutante(Pessoa):
      olhos = 3 #sobrescrevendo a Classe Pessoa modificando o atributo olhos para 3
 
 if __name__ == '__main__':
-    Vladimir = Mutante(nome='Vladimir', idade=25)
-    luciano = Homem(Vladimir, nome='Luciano')
-    print(Pessoa.cumprimentar(luciano))
+    renzo = Mutante(nome='Renzo')
+    luciano = Homem(renzo, nome='Luciano')
+    print(Pessoa.comprimentar)
     print(id(luciano))
-    print(luciano.cumprimentar())
+    print(luciano.comprimentar())
     print(luciano.nome)
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome,filho.idade)
-    luciano.sobrenome = 'Serra' #atributo dinâmico
+    luciano.sobrenome = 'Ramalho' #atributo dinâmico
     del  luciano.filhos #removendo um atributo dinamicamente
     luciano.olhos = 1
     del luciano.olhos
     print(luciano.__dict__) # confere quais os atributos de instancia do objeto
-    print(Vladimir.__dict__)
+    print(renzo.__dict__)
     # Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(luciano.olhos)
-    print(Vladimir.olhos)
-    print(id(Pessoa.olhos), id(luciano.olhos), id(Vladimir.olhos))
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
     print(Pessoa.metodo_estatico())
     print(luciano.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(),luciano.nome_e_atributos_de_classe())
     pessoa = Pessoa('Anonimo')
     print(isinstance(pessoa,Pessoa))
     print(isinstance(pessoa, Homem))
-    print(isinstance(Vladimir, Pessoa))
-    print(isinstance(Vladimir, Homem))
-    print(Vladimir.olhos)
+    print(isinstance(renzo, Pessoa))
+    print(isinstance(renzo, Homem))
+    print(renzo.olhos)
+    print(renzo.comprimentar)
+    print(luciano.comprimentar())
+
